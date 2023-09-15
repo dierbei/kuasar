d44255daefa3a4333de390f09e2d1fafbd376313
dierbei <1628652790@qq.com>
2023-09-15 07:14:35 +0000

    add workflow release
    Signed-off-by: dierbei <1628652790@qq.com>

---
+++ scripts/build/cargo-vendor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright 2022 The Kuasar Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+gh release create $RELEASE_VERSION --generate-notes
+
+directories=(
+    "quark"
+    "shim"
+    "vmm/sandbox"
+    "vmm/task"
+    vmm/common
+    "wasm"
+)
+
+for dir in "${directories[@]}"; do
+    (cd "$dir" && cargo vendor)
+done
+
+mkdir ../temp
+cp -r ./* ../temp/
+tar -czvf $VENDOR_NAME -C ../temp .
+gh release upload $RELEASE_VERSION $VENDOR_NAME
