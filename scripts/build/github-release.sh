a75bbd5763551f95aa3eed7b11c3d9e411383eaf
dierbei <1628652790@qq.com>
2023-10-08 11:59:34 +0000

    fix
    Signed-off-by: dierbei <1628652790@qq.com>

--- scripts/build/github-release.sh
+++ scripts/build/github-release.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 mkdir -p _dist
 find _artifacts -type f | xargs -I {} cp {} _dist/
 tar -czvf $ARCHIVE_NAME -C _dist .
